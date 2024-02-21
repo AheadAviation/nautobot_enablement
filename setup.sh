fd2773d75da17f6b27a5da731f8a205701b1b558
nobod <brian.nelson@ahead.com>
2024-02-20 17:49:48 -0700

    fixed some of the setup

--- setup.sh
+++ setup.sh
@@ -3,18 +3,18 @@
 # Detect the processor architecture
 ARCH=$(uname -m)
 
-# Initialize branch variable
-branch="main"
+# Use the 'branch' environment variable if set, otherwise default to 'main'
+branch="${branch:-main}"
 
 # Execute architecture-specific script based on detected architecture
 case "$ARCH" in
     x86_64)
         echo "Detected x86_64 architecture. Executing x86_64-specific script."
-        curl -sSL https://raw.githubusercontent.com/AheadAviation/nautobot_enablement/${branch}/setup_x86_64.sh | bash
+        curl -sSL "https://raw.githubusercontent.com/AheadAviation/nautobot_enablement/${branch}/setup_x86_64.sh" | bash
         ;;
     arm64)
         echo "Detected arm64 architecture. Executing arm64-specific script."
-        curl -sSL https://raw.githubusercontent.com/AheadAviation/nautobot_enablement/${branch}/setup_arm64.sh | bash
+        curl -sSL "https://raw.githubusercontent.com/AheadAviation/nautobot_enablement/${branch}/setup_arm64.sh" | bash
         ;;
     *)
         echo "Unsupported architecture: $ARCH"
